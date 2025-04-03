36f7be0dd4fdcedd249149128fa32f8630e4643e
Hugo Astier <slavboxx@gmail.com>
2025-04-03 10:29:54 +0200

    Ajoute PrimeNG et améliore l'interface des tâches

--- src/app/features/tasks/task-list/task-list.component.ts
+++ src/app/features/tasks/task-list/task-list.component.ts
@@ -3,11 +3,12 @@ import {CommonModule} from '@angular/common';
 import {TaskService} from '../task.service';
 import {Task} from '../../../models/task.model';
 import {mockTasks} from './mock-tasks';
+import {CardModule} from 'primeng/card';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardModule],
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
